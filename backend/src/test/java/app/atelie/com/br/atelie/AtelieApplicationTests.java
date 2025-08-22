95a2fe523672be519f990e13c3dcb7fdf23fda38
1GuiSouza1 <guilhermesza123@gmail.com>
2025-08-21 21:59:37 -0300

    Up project

---
+++ backend/src/test/java/app/atelie/com/br/atelie/AtelieApplicationTests.java
@@ -0,0 +1,13 @@
+package app.atelie.com.br.atelie;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AtelieApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

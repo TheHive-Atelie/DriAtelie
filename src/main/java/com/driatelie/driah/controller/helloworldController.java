0fe9aef686c1cfedaa145df6252cfc4cba7c7eaa
Hush9K <afonsosprocatti@gmail.com>
2025-09-11 23:29:10 -0300

    hotfix 2.0

---
+++ src/main/java/com/driatelie/driah/controller/helloworldController.java
@@ -0,0 +1,13 @@
+package com.driatelie.driah.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/gudinite")
+public class helloworldController {
+    @RequestMapping
+    public String hello() {
+        return "Good night!";
+    }
+}
---
+++ src/test/java/com/driatelie/driah/DriahApplicationTests.java
@@ -0,0 +1,13 @@
+package com.driatelie.driah;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DriahApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
